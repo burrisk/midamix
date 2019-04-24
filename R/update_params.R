446eb183505bae7c44bc67e4b8dd40c4a1837fa7
Kyle Burris <kyle.burris@duke.edu>
2019-04-24 13:53:58 -0400

    First crack at updating all parameters for observed data

--- R/update_params.R
+++ R/update_params.R
@@ -1,10 +1,13 @@
+#' @importFrom magrittr "%>%"
+
 update_alpha <- function(model_params, hyperpars){
   a_alpha <- hyperpars$a_alpha
   b_alpha <- hyperpars$b_alpha
   cluster_probs <- model_params$cluster_probs
   K <- length(cluster_probs)
   alpha <- rgamma(1, a_alpha + K - 1, b_alpha - log(cluster_probs[K]))
-  alpha
+  model_params$alpha <- alpha
+  model_params
 }
 
 update_v <- function(model_params, hyperpars){
@@ -17,7 +20,8 @@ update_v <- function(model_params, hyperpars){
     later_cluster <- sum(clusters > index)
     V[index] <- rbeta(1, 1 + in_cluster, alpha + later_cluster)
   }
-  V
+  model_params$V <- V
+  model_params
 }
 
 calculate_cluster_probs <- function(model_params){
@@ -30,23 +34,23 @@ calculate_cluster_probs <- function(model_params){
     cum_prod <- cum_prod * (1 - V[k])
   }
   cluster_probs
+  model_params$cluster_probs <- cluster_probs
+  model_params
 }
 
 update_sigma <- function(model_params, hyperpars){
   nu <- hyperpars$nu
   omega <- hyperpars$omega
   Sigma0 <- hyperpars$Sigma0
-  cluster_covs <- model_params$cluster_covs
-  cluster_precs <- cluster_covs
-  p <- dim(cluster_cov)[2]
-  K <- dim(cluster_cov)[3]
-  for (i in 1:K){
-    cluster_precs[, , i] <- solve(cluster_covs[, , i])
-  }
+  cluster_precs <- model_params$cluster_precs
+  p <- dim(cluster_precs)[2]
+  K <- dim(cluster_precs)[3]
   S <- solve(solve(Sigma0) + apply(cluster_precs, c(1, 2), sum))
   df <- nu * K + omega
   Sigma <- rwish(df, S)
   Sigma
+  model_params$Sigma <- Sigma
+  model_params
 }
 
 update_tau <- function(model_params, hyperpars){
@@ -64,7 +68,8 @@ update_tau <- function(model_params, hyperpars){
     colSums(X ^ 2)
   }))
   tau <- rgamma(p, a_tau + q_star * K / 2, b_tau + ss / 2)
-  tau
+  model_params$tau <- tau
+  model_params
 }
 
 update_b0 <- function(model_params, hyperpars){
@@ -78,13 +83,14 @@ update_b0 <- function(model_params, hyperpars){
   Omega <- diag(1 / tau)
   covar <- solve(Omega * K + diag(p) / sigma2b)
   B0 <- sum_cluster %*% Omega %*% covar + rmvnorm(q, rep(0, p), covar)
-  B0
+  model_params$B0 <- B0
+  model_params
 }
 
 update_clusters <- function(model_params, hyperpars){
   cluster_probs <- model_params$cluster_probs
   cluster_coefs <- model_params$cluster_coefs
-  cluster_covs <- model_params$cluster_covs
+  cluster_precs <- model_params$cluster_precs
   Z <- model_params$Z
   X <- model_params$X
   n <- nrow(Z)
@@ -96,7 +102,7 @@ update_clusters <- function(model_params, hyperpars){
     Zi <- Z[i, ]
     log_density <- rep(0, K)
     for (k in 1:K){
-      log_density[k] <- dmvnorm(Zi, Xi %*% cluster_coefs[, , k], cluster_covs[, , k],
+      log_density[k] <- dmvnorm_prec(Zi, Xi %*% cluster_coefs[, , k], cluster_precs[, , k],
                                 log = T)
     }
     log_probs <- log(cluster_probs) + log_density
@@ -104,17 +110,142 @@ update_clusters <- function(model_params, hyperpars){
     probs <- exp(log_probs) / sum(exp(log_probs))
     clusters[i] <- sample(1:K, 1, replace = F, prob = probs)
   }
-  clusters
+  model_params$clusters <- clusters
+  model_params
 }
 
-update_cluster_coefs <- function(model_params, hyperpars, conditionals){
-
+update_cluster_coefs <- function(model_params, hyperpars){
+  B0 <- model_params$B0
+  clusters <- model_params$clusters
+  Z <- model_params$Z
+  cluster_coefs <- model_params$cluster_coefs
+  cluster_covs <- model_params$cluster_covs
+  cluster_precs <- model_params$cluster_precs
+  tau <- model_params$tau
+  Omega <- diag(1 / tau)
+  X <- model_params$X
+  K <- dim(cluster_precs)[3]
+  p <- dim(cluster_precs)[2]
+  q <- ncol(X)
+  for (k in 1:K){
+    cluster_indices <- which(clusters == k)
+    if (length(cluster_indices) == 0){
+      cluster_coefs[, , k] <- B0 + rmvnorm(q, rep(0, p), Omega)
+    } else{
+      Z_cluster <- Z[cluster_indices, , drop = F]
+      X_cluster <- X[cluster_indices, , drop = F]
+      cov <- cluster_covs[, , k]
+      prec <- cluster_precs[, , k]
+      for (j in 1:p){
+        Sigma12 <- cov[j, -j, drop = F]
+        Sigma22_inv <- solve(cov[-j, -j, drop = F])
+        sigma_j_tilde <- (cov[j, j] - Sigma12 %*% Sigma22_inv %*% t(Sigma12))[1, 1]
+        z_tilde <- Z_cluster[, j, drop = F] - t(Sigma12 %*% Sigma22_inv %*%
+          t(Z_cluster[, -j, drop = F] - X_cluster %*% cluster_coefs[, -j, k, drop = F]))
+        var_kj <- solve(tau[j] * diag(q) + t(X_cluster) %*% X_cluster / sigma_j_tilde)
+        mean_kj <- var_kj %*% (tau[j] * B0[, j, drop = F] + t(X_cluster) %*% z_tilde /
+                                 sigma_j_tilde)
+        cluster_coefs[, j , k] <- rmvnorm(q, mean = mean_kj, sigma = var_kj)
+      }
+    }
+  }
+  cluster_coefs
+  model_params$cluster_coefs <- cluster_coefs
+  model_params
 }
 
 update_cluster_covs <- function(model_params, hyperpars){
-
+   cluster_coefs <- model_params$cluster_coefs
+   cluster_covs <- model_params$cluster_covs
+   cluster_precs <- model_params$cluster_precs
+   p <- dim(cluster_covs)[2]
+   Sigma <- model_params$Sigma
+   X <- model_params$X
+   Z <- model_params$Z
+   nu <- hyperpars$nu
+   for (k in 1:K){
+     cluster_indices <- which(clusters == k)
+     if (length(cluster_indices) == 0){
+       cluster_covs[, , k] <- riwish(nu, Sigma)
+       cluster_precs[, , k] <- solve(cluster_covs[, , k])
+     } else{
+       n_k <- length(cluster_indices)
+       Z_cluster <- Z[cluster_indices, , drop = F]
+       X_cluster <- X[cluster_indices, , drop = F]
+       M <- X_cluster %*% cluster_coefs[, , k]
+       Zmm <- Z_cluster - M
+       S <- matrix(0, nrow = p, ncol = p)
+       for (i in 1:n_k){
+         S = S + t(Zmm[i, , drop = F]) %*% Zmm[i, , drop = F]
+       }
+       cluster_covs[, , k] <- riwish(nu + n_k, Sigma + S)
+       cluster_precs[, , k] <- solve(cluster_covs[, , k])
+     }
+   }
+   model_params$cluster_covs <- cluster_covs
+   model_params$cluster_precs <- cluster_precs
+   model_params
 }
 
-update_zobs <- function(model_params, hyperpars, conditionals){
+update_zobs <- function(model_params, hyperpars, transformations){
+  Y <- model_params$Y
+  E <- is.na(Y)
+  Z <- model_params$Z
+  X <- model_params$X
+  clusters <- model_params$clusters
+  cluster_coefs <- model_params$cluster_coefs
+  cluster_covs <- model_params$cluster_covs
+  n <- nrow(Z)
+  p <- ncol(Z)
+  K <- dim(cluster_coefs)[3]
+  for (k in 1:K){
+    coefs <- matrix(cluster_coefs[, , k], ncol = p)
+    cov <- cluster_covs[, , k]
+    prec <- cluster_precs[, , k]
+    for (j in 1:p){
+      obs_indices <- which(clusters == k & E[, j] == 0)
+      if (length(obs_indices) == 0){
+        next
+      }
+      Y_obs <- Y[obs_indices, , drop = F]
+      Z_obs <- Z[obs_indices, , drop = F]
+      X_obs <- X[obs_indices, , drop = F]
+      boundsZ <- transformations$inverse_funs[[j]](Y_obs[, j])
+      obs_discrete <- which(boundsZ[,1] != boundsZ[,2])
+      if (length(obs_discrete) == 0){
+        Z[obs_indices, j] <- boundsZ[, 1]
+        next
+      }
+      X_obs_disc <- X_obs[obs_discrete, , drop = F]
+      Z_obs_disc <- Z_obs[obs_discrete, , drop = F]
+      Sigma12 <- cov[j, -j, drop = F]
+      Sigma22_inv <- solve(cov[-j, -j, drop = F])
+      cond_var <- sigma_j_tilde <- (cov[j, j] - Sigma12 %*% Sigma22_inv %*% t(Sigma12))[1, 1]
+      cond_mean <- X_obs_disc %*% coefs[, j, drop = F] - t(Sigma12 %*% Sigma22_inv %*%
+                                                t(Z_obs_disc[, -j, drop = F] -
+                                                    X_obs_disc %*% coefs[, -j, drop = F]))
+      Z[obs_indices[obs_discrete], j] <- truncnorm::rtruncnorm(length(obs_discrete),
+                                                        a = boundsZ[obs_discrete, 1],
+                                                        b = boundsZ[obs_discrete, 2],
+                                                        mean = cond_mean,
+                                                        sd = sqrt(cond_var))
+    }
+  }
+  model_params$Z <- Z
+  model_params
+}
 
+update_model_params <- function(model_params, hyperpars, transformations){
+  model_params <- model_params %>%
+    update_zobs(hyperpars, transformations) %>%
+    update_cluster_coefs(hyperpars) %>%
+    update_cluster_covs(hyperpars) %>%
+    update_clusters(hyperpars) %>%
+    update_b0(hyperpars) %>%
+    update_sigma(hyperpars) %>%
+    update_v(hyperpars) %>%
+    calculate_cluster_probs() %>%
+    update_tau(hyperpars) %>%
+    update_alpha(hyperpars)
+  model_params
 }
