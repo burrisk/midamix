71a89fa22a749fd0e3ba0986af344c01ce30a8ea
Kyle Burris <kyle.burris@duke.edu>
2019-05-03 14:59:52 -0400

    Added Alabama ACS data

---
+++ R/alabama.R
@@ -0,0 +1,16 @@
+#' American community survey data from the state of Alabama.
+#'
+#' A public-use dataset containing information about a 1 percent subsample of Alabama
+#' residents in the American community survey.
+#'
+#' @format A data frame with 46847 rows and 7 variables:
+#' \describe{
+#'   \item{Age}{age of the individual, in years}
+#'   \item{Medicare}{an indicator for whether the individual receives Medicare benefits}
+#'   \item{Medicaid}{an indicator for whether the individual receives Medicaid benefits}
+#'   \item{Disability}{an indicator for whether the individual has a disability}
+#'   \item{Educ}{level of education from 0 (under 3 years old) to 24 (doctorate)}
+#'   \item{Income}{annual income of the individual, in US dollars}
+#'   \item{HrsWorked}{number of hours worked per week}
+#' }
+"alabama"
---
+++ data-raw/alabama.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+
+alabama <- read_csv("data-raw/bama_raw.csv")
+
+na_to_zero <- function(x){
+  ifelse(is.na(x), 0, x)
+}
+
+na_to_one <- function(x){
+  ifelse(is.na(x), 1, x)
+}
+
+zeroed_variables <- c("Income", "HrsWorked")
+oned_variables <- c("Educ")
+
+alabama <- alabama %>%
+  mutate(Age = as.integer(Age),
+         Educ = as.integer(Educ),
+         Income = as.integer(Income),
+         HrsWorked = as.integer(HrsWorked),
+         Medicare = case_when(
+           Medicare == 1 ~ 1,
+           Medicare == 2 ~ 0
+         ),
+         Disability = case_when(
+           Disability == 1 ~ 1,
+           Disability == 2 ~ 0
+         ),
+         Medicaid = case_when(
+           Medicaid == 2 ~ 0,
+           Medicaid == 1 ~ 1
+         )) %>%
+  mutate_at(zeroed_variables, na_to_zero) %>%
+  mutate_at(oned_variables, na_to_one) %>%
+  dplyr::filter(Income >= 0, Age >= 65 | Medicare == 0 | Disability == 1)
+
+usethis::use_data(alabama)
